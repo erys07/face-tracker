4f453e5daeb37149944542c4400ab419c2dd57b8
Erivaldo <cavalcanteerivaldo0@gmail.com>
2024-11-24 18:30:47 -0300

    fix bug url

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 import cv2
 import numpy as np
 import mediapipe as mp
+import requests
 
 app = Flask(__name__)
+
 
 def calculate_face_asymmetry(landmarks):
     left_indices = [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -20,14 +22,23 @@ def calculate_face_asymmetry(landmarks):
     asymmetry_percentage = (difference / max(left_y_avg, right_y_avg)) * 100
     return asymmetry_percentage
 
+
 @app.route('/face-asymmetry', methods=['POST'])
 def handler():
     if request.method == "POST":
-        file = request.files["image"]
-        file_path = os.path.join(os.getcwd(), "face_image.jpg")
-        file.save(file_path)
+        image_url = request.json.get("image_url")
 
-        image = cv2.imread(file_path)
+        if not image_url:
+            return jsonify({"error": "URL da imagem não fornecida."}), 400
+
+        # Baixar a imagem da URL
+        try:
+            response = requests.get(image_url)
+            image = np.array(bytearray(response.content), dtype=np.uint8)
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        except Exception as e:
+            return jsonify({"error": f"Falha ao baixar a imagem: {str(e)}"}), 500
+
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         mp_face_mesh = mp.solutions.face_mesh
@@ -55,6 +66,7 @@ def handler():
         return jsonify(response)
     else:
         return jsonify({"error": "Método não permitido"}), 405
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
